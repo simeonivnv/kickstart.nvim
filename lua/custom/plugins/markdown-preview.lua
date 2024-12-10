ae30d7eab649071b3cd332135fdaf329a18fd724
Simeon Ivanov <isimeon@dhcp-10-6-175-225.ams.broadcom.net>
2024-12-10 07:36:57 +0100

    Open markdown port

--- lua/custom/plugins/markdown-preview.lua
+++ lua/custom/plugins/markdown-preview.lua
@@ -4,6 +4,9 @@ return {
   build = 'cd app && npm install',
   init = function()
     vim.g.mkdp_filetypes = { 'markdown' }
+    vim.g.mkdp_open_to_the_world = 1
+    vim.g.mkdp_echo_preview_url = 1
+    vim.g.mkdp_port = 5480
   end,
   ft = { 'markdown' },
 }
