ccbd8749d8a67d15bb8746eeac60e6a3095e7c39
Simeon Ivanov <isimeon@dhcp-10-6-175-225.ams.broadcom.net>
2025-02-07 11:28:22 +0100

    Add check for nil

--- init.lua
+++ init.lua
@@ -212,15 +212,17 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 local function format_hunks(bufnr)
   local hunks = require("gitsigns").get_hunks(bufnr)
   local format = require("conform").format
-  for i = #hunks, 1, -1 do
-    local hunk = hunks[i]
-    if hunk ~= nil and hunk.type ~= "delete" then
-      local start = hunk.added.start
-      local last = start + hunk.added.count
-      -- nvim_buf_get_lines uses zero-based indexing -> subtract from last
-      local last_hunk_line = vim.api.nvim_buf_get_lines(0, last - 2, last - 1, true)[1]
-      local range = { start = { start, 0 }, ["end"] = { last - 1, last_hunk_line:len() } }
-      format({ range = range })
+  if hunks ~= nil then
+    for i = #hunks, 1, -1 do
+      local hunk = hunks[i]
+      if hunk ~= nil and hunk.type ~= 'delete' then
+        local start = hunk.added.start
+        local last = start + hunk.added.count
+        -- nvim_buf_get_lines uses zero-based indexing -> subtract from last
+        local last_hunk_line = vim.api.nvim_buf_get_lines(0, last - 2, last - 1, true)[1]
+        local range = { start = { start, 0 }, ['end'] = { last - 1, last_hunk_line:len() } }
+        format { range = range }
+      end
     end
   end
 end
