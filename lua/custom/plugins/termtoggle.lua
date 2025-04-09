96fc8c5bf2f486bb22da84c51d4822cf8cc5b579
si014014 <simeon.ivanov@broadcom.com>
2025-04-09 11:54:40 +0200

    Some updates

--- lua/custom/plugins/termtoggle.lua
+++ lua/custom/plugins/termtoggle.lua
@@ -3,7 +3,7 @@ return {
   -- branch = 'master',
   cmd = 'ToggleTerm',
   keys = {
-    { '<A-i>', ':ToggleTerm direction=horizontal<CR>', { desc = 'Toogle Term' } },
+    { '<A-i>', ':ToggleTerm direction=float<CR>', { desc = 'Toogle Term' } },
   },
   config = true,
 }
